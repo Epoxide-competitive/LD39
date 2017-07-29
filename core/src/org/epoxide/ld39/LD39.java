e55e22f42a8ba034d692106760c341646d340d95
lclc98 <liam.c14785@gmail.com>
2017-07-29 12:24:56 +1000

    Initial loop

--- core/src/org/epoxide/ld39/LD39.java
+++ core/src/org/epoxide/ld39/LD39.java
@@ -5,10 +5,15 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class LD39 extends ApplicationAdapter {
     SpriteBatch batch;
     Texture img;
+
+    private static double STEP = 1d / 120d;
+    private static double prevTime;
+    private static double accumulator = 0;
 
     @Override
     public void create() {
@@ -18,11 +23,31 @@ public class LD39 extends ApplicationAdapter {
 
     @Override
     public void render() {
+        double currentTime = TimeUtils.millis() / 1000.0;
+        double frameTime = Math.min(currentTime - prevTime, 0.25);
+        final float delta = (float) frameTime;
+
+        prevTime = currentTime;
+        accumulator += frameTime;
+
+        while (accumulator >= STEP) {
+            accumulator -= STEP;
+            updateGame(delta);
+        }
+
+        renderGame(delta);
+    }
+
+    private void renderGame(float delta){
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         batch.draw(img, 0, 0);
         batch.end();
+    }
+
+    private void updateGame (float delta) {
+
     }
 
     @Override
--- desktop/src/org/epoxide/ld39/desktop/DesktopLauncher.java
+++ desktop/src/org/epoxide/ld39/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import org.epoxide.ld39.LD39;
 public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.vSyncEnabled = false;
+        config.foregroundFPS = -1;
         new LwjglApplication(new LD39(), config);
     }
 }
