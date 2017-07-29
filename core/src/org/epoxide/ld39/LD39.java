2377c2df4b2a17d707a219ec50fb5f9ee8166c94
lclc98 <liam.c14785@gmail.com>
2017-07-29 12:41:19 +1000

    Added render manager

--- core/src/org/epoxide/ld39/LD39.java
+++ core/src/org/epoxide/ld39/LD39.java
@@ -3,22 +3,28 @@ package org.epoxide.ld39;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.TimeUtils;
 
 public class LD39 extends ApplicationAdapter {
-    SpriteBatch batch;
-    Texture img;
 
-    private static double STEP = 1d / 120d;
-    private static double prevTime;
-    private static double accumulator = 0;
+    public static LD39 INSTANCE;
+    private double STEP = 1d / 120d;
+    private double prevTime;
+    private double accumulator = 0;
+
+    private SpriteBatch batch;
+    private OrthographicCamera camera;
+    private RenderManager renderManager;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
+        LD39.INSTANCE = this;
+
+        this.batch = new SpriteBatch();
+        this.camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        this.renderManager = new RenderManager();
     }
 
     @Override
@@ -38,21 +44,19 @@ public class LD39 extends ApplicationAdapter {
         renderGame(delta);
     }
 
-    private void renderGame(float delta){
+    private void renderGame(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
+
+        this.batch.setProjectionMatrix(this.camera.combined);
+
     }
 
-    private void updateGame (float delta) {
+    private void updateGame(float delta) {
 
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        img.dispose();
     }
 }
