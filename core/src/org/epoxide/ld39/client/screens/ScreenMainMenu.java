25a9daa7da213ec286e5a73afa1be225e2244b0e
Jared <jaredlll08@gmail.com>
2017-07-31 23:05:08 +0200

    Fixed ghost text

--- core/src/org/epoxide/ld39/client/screens/ScreenMainMenu.java
+++ core/src/org/epoxide/ld39/client/screens/ScreenMainMenu.java
@@ -1,6 +1,7 @@
 package org.epoxide.ld39.client.screens;
 
 import com.badlogic.gdx.*;
+import com.badlogic.gdx.graphics.GL20;
 import org.epoxide.ld39.LD39;
 
 public class ScreenMainMenu implements Screen {
@@ -17,6 +18,10 @@ public class ScreenMainMenu implements Screen {
     
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 0);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.getCamera().update();
+        game.getBatch().setProjectionMatrix(game.getCamera().combined);
         game.getBatch().begin();
         game.getFont().draw(game.getBatch(), "LD39", game.getCamera().position.x, game.getCamera().position.y);
         game.getFont().draw(game.getBatch(), "Press SPACE to start", game.getCamera().position.x, game.getCamera().position.y-20);
@@ -30,6 +35,9 @@ public class ScreenMainMenu implements Screen {
     
     @Override
     public void resize(int width, int height) {
+        game.getCamera().setToOrtho(false, width, height);
+        game.getCamera().update();
+        game.getLightMap().adjustSize(width, height);
     }
     
     @Override
