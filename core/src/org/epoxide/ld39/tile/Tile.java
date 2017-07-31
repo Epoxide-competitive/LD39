a99d2f295327c04731a96266d51c502f406ecd20
Tyler Hancock <darklime@live.ca>
2017-07-30 21:02:39 -0600

    Switched tile textures to ints rather than float. Float still supported.

--- core/src/org/epoxide/ld39/tile/Tile.java
+++ core/src/org/epoxide/ld39/tile/Tile.java
@@ -10,9 +10,9 @@ public class Tile extends Registerable<Tile> {
     private static final NamedRegistry<Tile> REGISTRY = new NamedRegistry<>();
 
     public static final Tile VOID = registerTile("void", new Tile());
-    public static final Tile WALL = registerTile("wall", new Tile()).setUV(0, 0, 0.0625f, 0.0625f);
-    public static final Tile FLOOR = registerTile("floor", new Tile()).setUV(0.0625f, 0, 0.125f, 0.0625f).setCollidable(true);
-    public static final Tile TORCH = registerTile("torch", new TileVisual()).setUV(0.25f, 0, 0.3125f, 0.0625f).setCollidable(true);
+    public static final Tile WALL = registerTile("wall", new Tile()).setUV(0, 0, 1, 1);
+    public static final Tile FLOOR = registerTile("floor", new Tile()).setUV(1, 0, 2, 1).setCollidable(true);
+    public static final Tile TORCH = registerTile("torch", new TileVisual()).setUV(4, 0, 5, 1).setCollidable(true);
 
     public float u, v, u2, v2;
     private boolean collidable = false;
@@ -22,6 +22,11 @@ public class Tile extends Registerable<Tile> {
         tile.setIdentifier(new Identifier(LD39.ID, id));
         REGISTRY.registerValue(tile);
         return tile;
+    }
+    
+    protected Tile setUV (int u, int v, int u2, int v2) {
+        
+        return setUV(u * 0.062f, v * 0.0625f, u2 * 0.0625f, v2 * 0.0625f);
     }
 
     protected Tile setUV (float u, float v, float u2, float v2) {
