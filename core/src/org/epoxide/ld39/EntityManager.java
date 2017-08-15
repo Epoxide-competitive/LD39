92b2c9d8ac368e26d83607681e7d901be7562bd0
Unknown <mattfdavies12@gmail.com>
2017-08-15 13:03:21 +0200

    Javadoc Rect
    Added inline javadoc for Rect
    Fixed logical errors in Rect.inYBounds
    Added Rect.setDimensions() for a unit square
    Added missing assignment to Rect(Vector2,Vector2)

--- core/src/org/epoxide/ld39/EntityManager.java
+++ core/src/org/epoxide/ld39/EntityManager.java
@@ -5,6 +5,11 @@ import org.epoxide.commons.registry.NamedRegistry;
 import org.epoxide.ld39.entity.Entity;
 import org.epoxide.ld39.entity.EntityComponent;
 import org.epoxide.ld39.entity.components.Sprite;
+import org.epoxide.ld39.util.JSONParser;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class EntityManager {
     public static final NamedRegistry<Entity> ENTITY_REGISTRY = new NamedRegistry<>();
@@ -12,8 +17,11 @@ public class EntityManager {
     private static final EntityComponent sprite = COMPONENT_REGISTRY.registerValue(new Sprite());
     //todo set up a component registry to query for component construction
     //todo call update on relevant Entities from this class
-    void init()
+    void deserializeEntities() throws IOException
     {
+        //TODO write json deserialization code to populate entity registry
+    }
+    void init(){
 
     }
     public void update(float delta)
