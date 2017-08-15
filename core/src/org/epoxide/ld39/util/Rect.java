b91971d1156eb9c9c996224f83a1f15613a91569
Unknown <mattfdavies12@gmail.com>
2017-08-15 13:03:21 +0200

    Rect fixes and Javadoc
    Added inline javadoc for Rect
    Fixed logical errors in Rect.inYBounds
    Added Rect.setDimensions() for a unit square
    Added missing assignment to Rect(Vector2,Vector2)

--- core/src/org/epoxide/ld39/util/Rect.java
+++ core/src/org/epoxide/ld39/util/Rect.java
@@ -2,12 +2,19 @@ package org.epoxide.ld39.util;
 
 import com.badlogic.gdx.math.Vector2;
 
+/**
+ *  Rectangle with the origin specified as it's centre rather than a corner
+ */
 public class Rect {
     //x,y are centre of rect
     private float x;
     private float y;
     private float length;
     private float height;
+
+    /**
+     * Constructs Rect with all values default initialised to 0 (0x0unit rectangle at the origin)
+     */
     public Rect()
     {
         x=0f;
@@ -15,6 +22,14 @@ public class Rect {
         length=0f;
         height=0f;
     }
+
+    /**
+     * Float based constructor
+     * @param centreX Global x-axis centre of the rectangle
+     * @param centreY Global y-axis centre of the rectangle
+     * @param length X-axis edge length of the rectangle
+     * @param height Y-axis edge length of the rectangle
+     */
     public Rect(float centreX, float centreY, float length, float height)
     {
         this.x = centreX;
@@ -22,36 +37,83 @@ public class Rect {
         this.length = length;
         this.height = height;
     }
+
+    /**
+     * Vector2 based constructor
+     * @param centre Centre of the rectangle
+     * @param edgeSize Edge length of rectangle
+     */
     public Rect(Vector2 centre, Vector2 edgeSize)
     {
         this.x = centre.x;
         this.y = centre.y;
+        this.length = edgeSize.x;
+        this.height = edgeSize.y;
     }
+
+    /**
+     * Float constructor which defaults to a 1x1 edge length
+     * @param centreX x-axis global centre
+     * @param centreY y-axis global centre
+     */
     public Rect(float centreX, float centreY)
     {
+        this.x = centreX;
+        this.y = centreY;
+        this.length = 1;
+        this.height = 1;
+    }
 
-    }
-    public boolean inBounds(float x, float y)
+    /**
+     * Float constructor which creates a square
+     * @param centreX Global x centre
+     * @param centreY Global y centre
+     * @param edgeLength Edge length of square
+     */
+    public Rect(float centreX, float centreY, float edgeLength)
     {
-        return inXRange(x) && inYRange(y);
+        this.x = centreX;
+        this.y = centreY;
+        this.length = edgeLength;
+        this.height = edgeLength;
     }
+
+    /**
+     * Detects if the specified point is within the bounds of this rect
+     * @param x x-axis coordinate to check
+     * @param y y-axis coordinate to check
+     * @return Whether the specified point is within or on this rectangle's bounds
+     */
+    public boolean inBounds(float x, float y) {return inXRange(x) && inYRange(y);}
+
+    /**
+     * Helper method to simplify inBounds for readability
+     * @param x x point to test
+     * @return if within x bounds
+     */
     private boolean inXRange(float x)
     {
         if (this.x == x)
             return true;
-        if(this.x > x && x < (this.x+(length/2)))
+        if(this.x > x && x <= (this.x+(length/2)))
                 return true;
-        if(this.x < x && x > (this.x-(length/2)))
+        if(this.x < x && x >= (this.x-(length/2)))
                 return true;
         return false;
     }
-    private boolean inYRange(float x)
+
+    /**
+     * Helper method to simplify inBounds for readability
+     * @param y y point to test
+     * @return if y is in the y bound
+     */
+    private boolean inYRange(float y)
     {
         if (this.y == y)
             return true;
-        if(this.y > y && y < (this.y+(height/2)))
+        if(this.y > y && y <= (this.y+(height/2)))
             return true;
-        if(this.y < y && y > (this.y-(height/2)))
+        if(this.y < y && y >= (this.y-(height/2)))
             return true;
         return false;
     }
@@ -63,6 +125,12 @@ public class Rect {
     {
         this.y = y;
     }
+
+    /**
+     * Shifts the centre of the rect to the specified global coordinates
+     * @param x x coordinate
+     * @param y y coordinate
+     */
     public void setCentre(float x, float y)
     {
         setCentreX(x);
@@ -72,8 +140,24 @@ public class Rect {
     {
         setCentre(centre.x,centre.y);
     }
+
+    /**
+     * Sets edge length to specified values
+     * @param length x-axis edge length
+     * @param height y-axis edge length
+     */
     public void setDimensions(float length, float height)
     {
         this.length = length;
+        this.height = height;
+    }
+
+    /**
+     * Sets edge length to 1 unit in both axis
+     */
+    public void setDimensions()
+    {
+        this.length = 1;
+        this.height = 1;
     }
 }
